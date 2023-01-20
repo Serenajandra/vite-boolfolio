a78bc82d934cec26dd7a04c6f580040cf9a8ea38
Serenajandra <serena.jandra@gmail.com>
2023-01-20 15:56:17 +0100

    aggiunta e collegata pagina AboutMe

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name:"AboutMe",
+}
+</script>
+<template>
+    <div class="container">
+        <h1>Chi sono</h1>
+    </div>
+
+</template>
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
+import AboutMe from "./pages/AboutMe.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -7,6 +8,11 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomePage,
+        },
+        {
+            path: "/about-me",
+            name: "aboutMe",
+            component: AboutMe,
         }
     ]
 });
